cb6bcf0d938b20d6baeda625bb4253309f1f61e4
Seren <cmingxuan94@gmail.com>
2018-08-22 17:23:43 +0800

    Refactor for command require deprecation

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const Command = require('command')
-
-module.exports = function customNpc(dispatch) {
+module.exports = function customNpc(mod) {
 	const config = require('./config.json'),
-		npcConfig = require('./npcConfig'),
-		command = Command(dispatch)
+		npcConfig = require('./npcConfig')
 		
 	let {enabled,
 		checkDespawn,
@@ -26,68 +23,68 @@ module.exports = function customNpc(dispatch) {
 	
 	
 /////Commands	
-	command.add('cn', (arg,arg2) => {
+	mod.command.add('cn', (arg,arg2) => {
 		if(arg === undefined) {
 			enabled = !enabled
-			command.message(`(Custom Npc) Module is ${enabled ? 'Enabled' : 'Disabled'}`)
+			mod.command.message(`(Custom Npc) Module is ${enabled ? 'Enabled' : 'Disabled'}`)
 			return
 		}
 		switch(arg.toLowerCase()) {
 			case 'owned':
 				changeOnlyPlayerSummons = !changeOnlyPlayerSummons
-				command.message(`(Custom Npc) changeOnlyPlayerSummons: ${changeOnlyPlayerSummons ? 'Enabled' : 'Disabled'}`)
+				mod.command.message(`(Custom Npc) changeOnlyPlayerSummons: ${changeOnlyPlayerSummons ? 'Enabled' : 'Disabled'}`)
 				break
 			case 'custom':
 				customChange = !customChange
-				command.message(`(Custom Npc) customChange: ${customChange ? 'Enabled' : 'Disabled'}`)
+				mod.command.message(`(Custom Npc) customChange: ${customChange ? 'Enabled' : 'Disabled'}`)
 				break
 			case 'despawn':
 				checkDespawn = !checkDespawn
 				changed = {}
-				command.message(`(Custom Npc) checkDespawn: ${checkDespawn ? 'Enabled' : 'Disabled'}`)
+				mod.command.message(`(Custom Npc) checkDespawn: ${checkDespawn ? 'Enabled' : 'Disabled'}`)
 				break
 			case 'random':
-				if(arg2 === undefined) command.message(`(Custom Npc) Wrong Command, missing second argument`)
+				if(arg2 === undefined) mod.command.message(`(Custom Npc) Wrong Command, missing second argument`)
 					
 				switch(arg2.toLowerCase()) {
 					case 'playersummon':
 					case 'player':
 					case 'owned':
 						changePlayerSummonsRandomly = !changePlayerSummonsRandomly
-						command.message(`(Custom Npc) changePlayerSummonsRandomly: ${changePlayerSummonsRandomly? 'Enabled' : 'Disabled'}`)
+						mod.command.message(`(Custom Npc) changePlayerSummonsRandomly: ${changePlayerSummonsRandomly? 'Enabled' : 'Disabled'}`)
 						break
 					case 'allsummon':
 					case 'allsummons':
 						changeAllSummonsRandomly = !changeAllSummonsRandomly
-						command.message(`(Custom Npc) changeAllSummonsRandomly: ${changeAllSummonsRandomly? 'Enabled' : 'Disabled'}`)
+						mod.command.message(`(Custom Npc) changeAllSummonsRandomly: ${changeAllSummonsRandomly? 'Enabled' : 'Disabled'}`)
 						break
 					case 'villager':
 					case 'townnpc':
 					case 'npc':
 						changeVillagers = !changeVillagers
-						command.message(`(Custom Npc) changeVillagers: ${changeVillagers? 'Enabled' : 'Disabled'}`)
+						mod.command.message(`(Custom Npc) changeVillagers: ${changeVillagers? 'Enabled' : 'Disabled'}`)
 						break
 					case 'all':
 					case 'everything':
 						changeEverything = !changeEverything
-						command.message(`(Custom Npc) changeEverything: ${changeEverything? 'Enabled' : 'Disabled'}`)
-						if(changeEverything) command.message(`(Custom Npc) This mode can bug your game out! Use at your own risk and your own discretion ;)`)
+						mod.command.message(`(Custom Npc) changeEverything: ${changeEverything? 'Enabled' : 'Disabled'}`)
+						if(changeEverything) mod.command.message(`(Custom Npc) This mode can bug your game out! Use at your own risk and your own discretion ;)`)
 						break
 					default:
-						command.message(`(Custom Npc) Wrong Command, error in second argument. (player/all/villager)`)
+						mod.command.message(`(Custom Npc) Wrong Command, error in second argument. (player/all/villager)`)
 				}
 				break
 			default:
-				command.message(`(Custom Npc) Invalid Command`)
+				mod.command.message(`(Custom Npc) Invalid Command`)
 		}
 	})
 	
 //////Hooks	
-	dispatch.hook('S_LOGIN', 10, (event) => {
+	mod.hook('S_LOGIN', 10, (event) => {
 		({gameId} = event)
 	})
 	
-	dispatch.hook('S_SPAWN_NPC', 9 , event => {
+	mod.hook('S_SPAWN_NPC', 9 , event => {
 		if(!enabled) return
 		
 		if(npcConfig[event.huntingZoneId] && npcConfig[event.huntingZoneId][event.templateId] && customChange) {//Have to do this else alot of errors
@@ -127,12 +124,12 @@ module.exports = function customNpc(dispatch) {
 		}
 	})
 	
-	dispatch.hook('S_LOAD_TOPO', 3 , event => {
+	mod.hook('S_LOAD_TOPO', 3 , event => {
 		inDungeon = event.zone > 9000
 		changed = {}
 	})
 	
-	dispatch.hook('S_DESPAWN_NPC', 3 , event => {
+	mod.hook('S_DESPAWN_NPC', 3 , event => {
 		if(!checkDespawn) return
 		
 		if(changed[event.gameId]) { 
